43bf3c6effe3a6c7a6b73d7c72ca96a7aac530ea
yzhu <yzhu@anigo.com.au>
2018-11-01 13:24:22 +1100

    initial commit

---
+++ FileProvider.Test/PhysicalFileProviderTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using Microsoft.Extensions.FileProviders;
+
+namespace FileProvider.Test
+{
+    [TestFixture]
+    public class PhysicalFileProviderTest
+    {
+        [TestCase("")]
+        [TestCase("./")]
+        public void ContructorInvalidTest(string path) {
+            Assert.Throws<ArgumentException>(
+                () => new PhysicalFileProvider(path)
+            );
+        }
+
+        [TestCase(@"c:\")]
+        [TestCase(@"c:\bin")]
+        public void ContructorValidTest(string path) {
+            using (var provider = new PhysicalFileProvider(path)) {
+                Assert.That(provider, Is.Not.Null);
+            }
+        }
+    }
+}
---
+++ FileProvider/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using com.zhusmelb.Util.Logging;
+using Microsoft.Extensions.FileProviders;
+
+namespace FileProvider
+{
+    public class Program
+    {
+        private static readonly ILogger _log 
+            = LogHelper.GetLogger(typeof(Class1).FullName);
+
+        public static void Main(string[] args) {
+            var provider = new PhysicalFileProvider(".");
+        }
+    }
+}

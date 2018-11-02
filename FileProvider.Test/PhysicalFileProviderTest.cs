842b89a67bbb1f34e3495f9854d9023a455b1ed8
yzhu <yzhu@anigo.com.au>
2018-11-02 12:28:43 +1100

    Add test SetUp and TearDown, and content test.

--- FileProvider.Test/PhysicalFileProviderTest.cs
+++ FileProvider.Test/PhysicalFileProviderTest.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+
 using NUnit.Framework;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.FileProviders.Physical;
+
 using com.zhusmelb.Util.Logging;
 
 namespace FileProvider.Test
@@ -9,8 +13,24 @@ namespace FileProvider.Test
     [TestFixture]
     public class PhysicalFileProviderTest
     {
+        private IFileProvider _fileProvider;
+        private const string Test_Assets_Path = "./Assets/";
         private static readonly ILogger _log 
             = LogHelper.GetLogger(typeof(PhysicalFileProviderTest).FullName);
+
+        [SetUp]
+        public void Init() {
+            var path = Path.GetFullPath(Test_Assets_Path);
+            Assert.That(Path.IsPathRooted(path), Is.True);
+            Assert.That(Directory.Exists(path), Is.True);
+            _fileProvider = new PhysicalFileProvider(Test_Assets_Path, ExclusionFilters.Sensitive);
+            Assert.That(_fileProvider, Is.Not.Null);
+        }
+
+        [TearDown]
+        public void Cleanup() {
+            (_fileProvider as IDisposable)?.Dispose();
+        }
 
         [TestCase("")]
         [TestCase("./")]
@@ -41,6 +61,7 @@ namespace FileProvider.Test
         }
 
         [Test]
+        [Ignore("Testing a bug in .NET Core 2.1.1")]
         public void ConstructorNonExistTest() {
             var path = Path.GetFullPath("./Assets/Folder2");
             _log.Info($"Full path of target folder {path}");
@@ -53,6 +74,31 @@ namespace FileProvider.Test
             Assert.Throws<DirectoryNotFoundException>(
                 () => new PhysicalFileProvider(path)
             );
+        }
+
+        [Test]
+        public void GetDirectoryContentsTest() {
+            var provider = _fileProvider;
+            _log.Info($"provider.Root: {provider?.Root}");
+            var contents = provider.GetDirectoryContents("\temp");
+            Assert.That(Object.ReferenceEquals(contents, NotFoundDirectoryContents.Singleton), Is.True);
+
+            contents = provider.GetDirectoryContents("");
+            Assert.That(contents.Exists, Is.True);
+            logFileInfo(contents);
+
+            contents = provider.GetDirectoryContents("Folder1");
+            Assert.That(contents.Exists, Is.True);
+            logFileInfo(contents);
+        }
+
+        private void logFileInfo(IEnumerable<IFileInfo> files) {
+            foreach (var fileInfo in files) {
+                if (!fileInfo.Exists)
+                    _log.Info($"{fileInfo.Name} does not exist.");
+                else 
+                    _log.Info($"{fileInfo.Name}, {(fileInfo.IsDirectory ? "Directory" : fileInfo.Length.ToString())}, {fileInfo.LastModified}, {fileInfo.PhysicalPath}");
+            }
         }
     }
 }
